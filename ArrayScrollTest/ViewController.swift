ae0eb378d678fc488041c5ca26847606805cb849
Emma Carpenetti <emmac4164@gmail.com>
2021-07-21 09:18:54 -0400

    notes added, saving before I start over

--- ArrayScrollTest/ViewController.swift
+++ ArrayScrollTest/ViewController.swift
@@ -13,7 +13,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    var array = ["Place 1", "Place 2", "Place 3", "Place 4"]
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+    }
+    
+    
 }
+
+// Options for final project resource lists:
+    // Labels inside of a scroll view
+    // Table view controller
+    // https://www.youtub.com/watch?v=asi9EzPgAJE
+    // embed list?
+    // best way: custom cells in a UI table view
 
